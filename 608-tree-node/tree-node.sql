93ff27d6fa037a5b90640534bf9e0864795f812f
Shraddha Sanap <shraddhasanap26276@gmail.com>
2026-04-09 20:29:09 +0530

    Time: 447 ms (68.81%) | Memory: 0B (100.00%) - LeetSync

---
+++ 608-tree-node/tree-node.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+SELECT id,
+CASE WHEN p_id IS NULL THEN 'Root'
+     WHEN id IN (  SELECT p_id FROM tree WHERE p_id IS NOT NULL) THEN 'Inner'
+     ELSE 'Leaf' END AS TYPE
+ FROM tree
